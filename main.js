e74338da3b28cb9ab8144c097ecd3a6ce5100f95
Osman Herman <osmanherman27@gmail.com>
2023-03-28 22:34:26 -0300

    Subida

---
+++ main.js
@@ -0,0 +1,10 @@
+document.querySelector(".hamb").addEventListener('click', function(e) {
+    document.getElementById('one').classList.toggle('oneClick')
+    document.getElementById('two').classList.toggle('twoClick')
+    document.getElementById('trhee').classList.toggle('trheeClick')
+    document.querySelector('.menu').classList.toggle('menuClick')
+    document.getElementById('img-1').classList.toggle('blurr')
+    document.getElementById('img-2').classList.toggle('blurr')
+    document.getElementById('img-3').classList.toggle('blurr')
+    document.getElementById('img-4').classList.toggle('blurr')
+})
